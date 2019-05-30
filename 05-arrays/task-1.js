26cc91d3f34c661a147fd5afe6a8e35e21e8d518
giorgi <g_shavishvili@cu.edu.ge>
2019-05-30 19:04:14 +0400

    feat(05-arrays): Finish 2 tasks

---
+++ 05-arrays/task-1.js
@@ -0,0 +1,19 @@
+const array = [1,2,3];
+
+
+function forEach(arr, fun) {
+if(arr && Array.isArray(arr) &&  fun && typeof fun === "function") {
+    for (var i=0; i<arr.length; i++) {
+        fun(arr[i] , i , arr);
+    }
+
+}else {
+    throw new Error("some error");
+}
+
+
+}
+
+forEach(array, function(item, i, arr) {
+
+});
---
+++ 05-arrays/task-2.js
@@ -0,0 +1,24 @@
+const array = [1,2,3];
+var number = 0;
+var newarray= [];
+
+
+function filter(arr, fun) {
+if(arr && Array.isArray(arr) &&  fun && typeof fun === "function") {
+    for (var i=0; i<arr.length; i++) {
+        if(fun(arr[i] , i , arr)) {
+            newarray.push(arr[i]);
+        };
+    }
+    console.log(newarray);
+
+}else {
+    throw new Error("some error");
+}
+
+
+}
+
+filter(array, function(item, i, arr) {
+    return item > 2;
+});
