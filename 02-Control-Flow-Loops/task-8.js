118bc66ad5e1d74ab4dc5467487b06229969b441
giorgi <g_shavishvili@cu.edu.ge>
2019-05-31 21:23:49 +0400

    fix(Fixed): Fixed

--- 02-Control-Flow-Loops/task-8.js
+++ 02-Control-Flow-Loops/task-8.js
@@ -1,9 +1,9 @@
-let arr = [1,2,3,4];
+let arr = [1,2,3,4,6];
 let sum = 0;
 
 for (let i=0; i <arr.length; i++){
 
-    if(arr[i] % 2 == 0 && i > 3){
+    if(arr[i] % 2 == 0 && arr[i] > 3){
         sum = sum + arr[i];
     }
 }
