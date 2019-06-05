f502c6e3c1f26bb82f033dc9089e8a64a5f36ee0
giorgi <g_shavishvili@cu.edu.ge>
2019-06-05 20:52:36 +0400

    feat(10-Object-Oriented-Programing-Part1): Finish task 1

---
+++ 10-Object-Oriented-Programing-Part1/task-1.js
@@ -0,0 +1,31 @@
+function CoffeeMachine(power) {
+    let timerId;
+	this.waterAmount = 0;
+	var WATER_HEAT_CAPACITY = 4200;
+	var self = this;
+    
+
+    function getBoilTime() {
+		return self.waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+	}
+	
+    function onReady() {
+		console.log('Coffee is ready');
+	}
+	
+    this.run = function() {
+        timerId = setTimeout(onReady, getBoilTime());
+    };
+    
+    this.stop = function() {
+        clearTimeout(timerId);
+        timerId = void 0 ;
+        console.log("coffee isn't ready");
+    }
+}
+
+
+var coffeeMachine = new CoffeeMachine(50000);
+coffeeMachine.waterAmount = 200;
+coffeeMachine.run();
+coffeeMachine.stop(); // coffee isn't ready
