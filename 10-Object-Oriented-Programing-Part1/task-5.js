55bd0a8755e2cff5a7d3c18fb10f8ccbe228bb71
giorgi <g_shavishvili@cu.edu.ge>
2019-06-07 19:56:58 +0400

    feat(10-Object-Oriented-Programing-Part1): Fixed all tasks

--- 10-Object-Oriented-Programing-Part1/task-5.js
+++ 10-Object-Oriented-Programing-Part1/task-5.js
@@ -1,52 +1,73 @@
 function CoffeeMachine(power, capacity) {
-    var waterAmount = 0;
-    var WATER_HEAT_CAPACITY = 4200;
-    let isProgress = false
-    let timerId; 
-  
-    function getTimeToBoil() {
-      return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
-    }
-  
-    this.setWaterAmount = function(amount) {
+  let waterAmount = 0;
+  let WATER_HEAT_CAPACITY = capacity;
+  let power1;
+  let timerId = void 0;
+  let self = this;
+
+  function getBoilTime() {
+      return self.waterAmount * WATER_HEAT_CAPACITY * 80 / power1;
+  }
+
+  this.setWaterAmount = function (amount) {
       if (amount < 0) {
-        throw new Error("Value has to be positive");
+          throw new Error("Value has to be positive.");
       }
       if (amount > capacity) {
-        throw new Error("You can't put more water, than " + capacity);
+          throw new Error("You can't put more water, than " + capacity);
       }
       waterAmount = amount;
-    };
-  
-    function onReady() {
-      console.log('Coffee is ready');
-      isProgress = false
-    }
-  
-    this.run = function() {
-      isProgress = true
-      setTimeout(onReady, getTimeToBoil());
-    };
-  
-    this.setOnReady = function(action) {
-      isProgress = true
-      setTimeout(function() {
-        action()
-        isProgress = false
-      }, getTimeToBoil());
-    };
-  
-    this.isRunning = function() {
-      return isProgress
-    }
-  
+  };
+
+  this.addWater = function (amount) {
+      this.setWaterAmount(amount + waterAmount)
   }
-  
-  var coffeeMachine = new CoffeeMachine(20000, 500);
-  coffeeMachine.setWaterAmount(100);
-  console.log('Before: ' + coffeeMachine.isRunning()); // Before: false
-  coffeeMachine.run();
-  console.log('In progress: ' + coffeeMachine.isRunning()); // In progress: true
-  coffeeMachine.setOnReady(function() {
-    console.log('After: ' + coffeeMachine.isRunning()); // After: false
-  });
+
+  this.getWaterAmount = function () {
+      return waterAmount;
+  };
+
+  this.getPower = function () {
+      return power1;
+  };
+
+  function onReady() {
+      console.log('Coffee is ready');
+  }
+
+  this.setOnReady = function (func) {
+      onReady = func;
+  }
+
+  this.run = function () {
+      timerId = setTimeout(() => {
+          timerId = null;
+          onReady();
+      }, getBoilTime());
+  };
+
+  this.isRunning = function () {
+      return !!timerId;
+  }
+
+  this.stop = function () {
+      if (typeof timerId !== 'undefined') {
+          clearTimeout(timerId);
+          timerId = void 0;
+          console.log(`coffee isn't ready`)
+      }
+  }
+}
+
+var coffeeMachine = new CoffeeMachine(20000, 500);
+coffeeMachine.setWaterAmount(100);
+
+console.log('Before: ' + coffeeMachine.isRunning()); // Before: false
+
+coffeeMachine.run();
+
+console.log('In progress: ' + coffeeMachine.isRunning()); // In progress: true
+
+coffeeMachine.setOnReady(function() {
+   console.log('After: ' + coffeeMachine.isRunning()); // After: false
+});
