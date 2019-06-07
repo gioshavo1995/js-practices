3a05298bca1fe86669467e72ffe1796e2ad1c113
giorgi <g_shavishvili@cu.edu.ge>
2019-06-07 21:42:31 +0400

    feat(11-Object oriented-programing-part2): Finish task 2

--- 11-Object-Oriented-Programing-Part2/task-2.js
+++ 11-Object-Oriented-Programing-Part2/task-2.js
@@ -1,21 +1,23 @@
 function CoffeeMachine(power) {
-	var waterAmount = 0;
-	var WATER_HEAT_CAPACITY = 4200;
-	
-    function getTimeToBoil() {
-		return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
-	}
-  	
-    this.run = function() {
-    	setTimeout(function() {
-			console.log('Coffee is ready!');
-    	}, getTimeToBoil());
-	};
-	this.setWaterAmount = function(amount) {
-		waterAmount = amount;
-	};
-}
-
-var coffeeMachine = new CoffeeMachine(10000);
-coffeeMachine.setWaterAmount(50);
-coffeeMachine.run();
+	this.waterAmount = 0;
+	this.WATER_HEAT_CAPACITY = 4200;
+	this.power = power
+ }
+ 
+ CoffeeMachine.prototype.getTimeToBoil = function () {
+	return this.waterAmount * this.WATER_HEAT_CAPACITY * 80 / this.power
+ }
+ 
+ CoffeeMachine.prototype.run = function () {
+	setTimeout(function () {
+		console.log('Coffee is ready');
+	}, this.getTimeToBoil());
+ };
+ 
+ CoffeeMachine.prototype.setWaterAmount = function (amount) {
+	this.waterAmount = amount;
+ };
+ 
+ let coffeeMachine = new CoffeeMachine(10000);
+ coffeeMachine.setWaterAmount(50);
+ coffeeMachine.run();
