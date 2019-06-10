967ef22681e889e064564fc73e7677894771b4bb
giorgi <g_shavishvili@cu.edu.ge>
2019-06-10 18:32:01 +0400

    feat(12-Javascript-classes): Fixed some tasks

--- 12-Javascript-classes/task-4.js
+++ 12-Javascript-classes/task-4.js
@@ -15,7 +15,7 @@ class MyString {
 
     let splitStr = str.toLowerCase().split(' ');
     for (let i = 0; i < splitStr.length; i++) {
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+        splitStr[i]= this.ucFirst(splitStr[i]);  
     }
     return splitStr.join(' '); 
 }
