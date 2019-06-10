9a445699879de33c63df5d8292b0f6fa147d03eb
giorgi <g_shavishvili@cu.edu.ge>
2019-06-10 18:18:57 +0400

    feat(12-Javascript-classes): Fixed 4 task

--- 12-Javascript-classes/task-4.js
+++ 12-Javascript-classes/task-4.js
@@ -1,9 +1,10 @@
 class MyString {
     reverse(str) {
-        let splitString = str.split("");
-        let reverseString = splitString.reverse();
-        let joinString = reverseString.join("");
-        return joinString;
+        var newString = "";
+        for (var i = str.length - 1; i >= 0; i--) { 
+            newString += str[i]; // or newString = newString + str[i];
+        }
+        return newString;
     }
 
     ucFirst(string) {
