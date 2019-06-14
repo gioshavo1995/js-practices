6bcdc3963566d5da8a17dc53090eb2921cb8b22f
giorgi shavishvili <g_shavishvili@cu.edu.ge>
2019-06-14 14:39:46 +0400

    fix(12-Introducing-Javascript-Classes): Fix 4 task

--- 12-Javascript-classes/task-4.js
+++ 12-Javascript-classes/task-4.js
@@ -1,7 +1,7 @@
 class MyString {
     reverse(str) {
-        var newString = "";
-        for (var i = str.length - 1; i >= 0; i--) { 
+        let newString = "";
+        for (let i = str.length - 1; i >= 0; i--) { 
             newString += str[i]; // or newString = newString + str[i];
         }
         return newString;
